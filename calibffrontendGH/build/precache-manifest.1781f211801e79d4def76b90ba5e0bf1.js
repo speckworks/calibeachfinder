8200f3a262ffc70151a63d69b2246f0c79ee4483
John Speck <speckworks@gmail.com>
2020-03-15 12:35:48 -0400

    style change

---
+++ calibffrontendGH/build/precache-manifest.1781f211801e79d4def76b90ba5e0bf1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4fd3ef01613c2fa9cbab9ef537cb98",
+    "url": "/calibeachfinder/index.html"
+  },
+  {
+    "revision": "eaaaf65146ccf562efad",
+    "url": "/calibeachfinder/static/css/main.37972e9b.chunk.css"
+  },
+  {
+    "revision": "676694a3c05615dfd739",
+    "url": "/calibeachfinder/static/js/2.9e1448ac.chunk.js"
+  },
+  {
+    "revision": "0865c46a6a3b33d9e8048eee6a36de4d",
+    "url": "/calibeachfinder/static/js/2.9e1448ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eaaaf65146ccf562efad",
+    "url": "/calibeachfinder/static/js/main.f0013044.chunk.js"
+  },
+  {
+    "revision": "ebd7e3c4d7fb6845804c",
+    "url": "/calibeachfinder/static/js/runtime-main.856f3923.js"
+  },
+  {
+    "revision": "9d759028b0749eb1cede1d9fe591f138",
+    "url": "/calibeachfinder/static/media/beach-background.9d759028.jpg"
+  },
+  {
+    "revision": "cfafeca245c271956684db156ca5571a",
+    "url": "/calibeachfinder/static/media/beach-marker.cfafeca2.png"
+  }
+]);
--- calibffrontendGH/build/service-worker.js
+++ calibffrontendGH/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calibeachfinder/precache-manifest.4c48655dea9037969761077cd427c56b.js"
+  "/calibeachfinder/precache-manifest.1781f211801e79d4def76b90ba5e0bf1.js"
 );
 
 self.addEventListener('message', (event) => {
